053af8b43a85893d7129fa3eb81a74702b308e7b
Tomino Project <tomaspikna@gmail.com>
2026-09-23 15:35:17 +0200

    Respect Android system safe areas in CSP app

--- mobile/csp-android/android/app/src/main/java/com/connectsportspro/app/MainActivity.java
+++ mobile/csp-android/android/app/src/main/java/com/connectsportspro/app/MainActivity.java
@@ -1,5 +1,29 @@
 package com.connectsportspro.app;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        View content = findViewById(android.R.id.content);
+        ViewCompat.setOnApplyWindowInsetsListener(content, (view, windowInsets) -> {
+            Insets systemBars = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return windowInsets;
+        });
+
+        ViewCompat.requestApplyInsets(content);
+    }
+}
